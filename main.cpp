2ad00f8ed138b24f5a4ca48a70868033cd278b6d
Mattias Nyberg <muthaias.uu@gmail.com>
2010-01-31 23:47:56 +0100

    The dialog box

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QtGui/QApplication>
+#include <QtGui/QFileDialog>
 #include "mainwindow.h"
 #include "displaywidget.h"
 #include "wavelettransform.h"
@@ -10,8 +11,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
+    QString fileName = QFileDialog::getOpenFileName(0, "Open sound file");
+  
     const char* soundfile = "file0255_2_in.wav";
-    boost::shared_ptr<WavelettTransform> wt( new WavelettTransform(soundfile) );
+    boost::shared_ptr<WavelettTransform> wt( new WavelettTransform(fileName.toAscii().constData()) );
     boost::shared_ptr<DisplayWidget> dw( new DisplayWidget( wt ) );
 
     w.setCentralWidget( dw.get() );
