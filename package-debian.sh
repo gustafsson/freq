e41eb715eca7649af7eaf1d854dfb7666a3d9268
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2010-04-01 11:23:20 +0200

    Package debian

--- package-debian.sh
+++ package-debian.sh
@@ -1,3 +1,7 @@
-#/bin/bash
+#!/bin/bash
+rm -f package-debian/DEBIAN/*~ && \
 cp sonicawe package-debian/opt/sonicawe && \
+pushd package-debian && \
+md5sum opt/sonicawe/sonicawe > DEBIAN/md5sums && \
+popd && \
 dpkg -b package-debian sonicawe_0.3.30_amd64.deb
