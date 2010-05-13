f59c470f5eb2f5da99279667a9796b43856c2e78
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2010-05-13 21:40:52 +0200

    First implementation of using boost graph library.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,9 @@
 #include "signal-operation-composite.h"
 #include "signal-microphonerecorder.h"
 #include "signal-audiofile.h"
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/unordered_set.hpp>
+#include <boost/graph/adjacency_iterator.hpp>
 
 #if defined(_MSC_VER)
 #define _USE_MATH_DEFINES
@@ -18,6 +21,7 @@
 #include <math.h>
 
 using namespace std;
+using namespace boost;
 
 MainWindow::MainWindow(const char* title, QWidget *parent)
     : QMainWindow(parent), ui(new Ui_MainWindow)
@@ -114,25 +118,27 @@ void MainWindow::connectLayerWindow(DisplayWidget *d)
     d->setWorkerSource();
 }
 
-void MainWindow::updateOperationsTree( Tfr::pFilter f, QTreeWidgetItem* w )
+struct TitleAndTooltip {
+    std::string title, tooltip;
+};
+
+typedef boost::adjacency_list<vecS,vecS, bidirectionalS, TitleAndTooltip> OperationGraph;
+const OperationGraph::vertex_descriptor vertex_descriptor_null = (OperationGraph::vertex_descriptor)-1;
+
+OperationGraph::vertex_descriptor populateGraph( Tfr::pFilter f, OperationGraph& graph )
 {
-    BOOST_ASSERT( w );
-
-    QTreeWidgetItem* child = new QTreeWidgetItem( 0 );
-
     stringstream title;
     stringstream tooltip;
     title << fixed << setprecision(1);
     tooltip << fixed << setprecision(2);
 
-    if ( Tfr::FilterChain* filter_chain = dynamic_cast<Tfr::FilterChain*>(f.get()))
+    Tfr::FilterChain* filter_chain=0;
+
+    if ( 0 != (filter_chain = dynamic_cast<Tfr::FilterChain*>(f.get())))
     {
         title << "Chain #" << filter_chain->size() << "";
         tooltip << "Chain contains " << filter_chain->size() << " subfilters";
 
-        BOOST_FOREACH( Tfr::pFilter f, *filter_chain ) {
-            updateOperationsTree( f, child );
-        }
     } else if (Tfr::EllipsFilter* c = dynamic_cast<Tfr::EllipsFilter*>(f.get())) {
             float r = fabsf(c->_t1-c->_t2);
             title << "Ellips [" << c->_t1-r << ", " << c->_t1 + r << "]";
@@ -151,35 +157,33 @@ void MainWindow::updateOperationsTree( Tfr::pFilter f, QTreeWidgetItem* w )
         title << "No filter";
     }
 
-    child->setText(0, QString::fromStdString( title.str()));
-    child->setToolTip(0, QString::fromStdString( tooltip.str()) );
-    child->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsSelectable | Qt::ItemIsEnabled);
-    child->setCheckState(0, Qt::Checked);
-    w->addChild( child );
+    TitleAndTooltip tat;
+    tat.title = title.str();
+    tat.tooltip = tooltip.str();
+
+    OperationGraph::vertex_descriptor v = add_vertex(tat, graph);
+
+    if (filter_chain) {
+        BOOST_FOREACH( Tfr::pFilter f, *filter_chain ) {
+            OperationGraph::vertex_descriptor c;
+            c = populateGraph( f, graph );
+
+            add_edge(v,c,graph);
+        }
+    }
+
+    return v;
 }
 
-void MainWindow::updateOperationsTree( Signal::pSource s )
+OperationGraph::vertex_descriptor populateGraph( Signal::pSource s, OperationGraph& graph )
 {
-    TaskTimer tt("Updating operations tree");
-
-    ui->operationsTree->clear();
-    QTreeWidgetItem* w = ui->operationsTree->invisibleRootItem();
-
-    updateOperationsTree( s, w );
-}
-
-
-void MainWindow::updateOperationsTree( Signal::pSource s, QTreeWidgetItem* w )
-{
-    BOOST_ASSERT( w );
-
-    QTreeWidgetItem* child = new QTreeWidgetItem( 0 );
-
     stringstream title;
     stringstream tooltip;
     title << fixed << setprecision(1);
     tooltip << fixed << setprecision(2);
 
+    Signal::pSource childSource;
+    Tfr::pFilter childFilter;
     if (Signal::MicrophoneRecorder* mic = dynamic_cast<Signal::MicrophoneRecorder*>(s.get()))
     {
         title << "Microphone";
@@ -195,20 +199,21 @@ void MainWindow::updateOperationsTree( Signal::pSource s, QTreeWidgetItem* w )
         title << "Filter";
         tooltip << "Filter Operation";
 
-        updateOperationsTree( filter_operation->filter(), child );
+        childFilter = filter_operation->filter();
     }
     else if (Signal::OperationSubOperations* sub_operations = dynamic_cast<Signal::OperationSubOperations*>(s.get()))
     {
         title << demangle( typeid(*s).name() );
         tooltip << "Composite operation" << demangle(typeid(*s).name());
 
-        updateOperationsTree( sub_operations->subSource(), child );
+        childSource = sub_operations->subSource();
     }
     else if (Signal::OperationSuperposition* super = dynamic_cast<Signal::OperationSuperposition*>(s.get()))
     {
         title << "Sum";
         tooltip << "Sum of two signals by superpositioning";
-        updateOperationsTree( super->source2(), child );
+
+        childSource = super->source2();
     }
     else
     {
@@ -216,15 +221,162 @@ void MainWindow::updateOperationsTree( Signal::pSource s, QTreeWidgetItem* w )
         tooltip << "Source not further described: " << demangle(typeid(*s).name());
     }
 
+    TitleAndTooltip tat;
+    tat.title = title.str();
+    tat.tooltip = tooltip.str();
 
-    child->setText(0, QString::fromStdString( title.str()));
-    child->setToolTip(0, QString::fromStdString( tooltip.str()) );
+    OperationGraph::vertex_descriptor v = add_vertex(tat, graph);
+
+    if ( Signal::Operation* op = dynamic_cast<Signal::Operation*>(s.get())) {
+        OperationGraph::vertex_descriptor c;
+        c = populateGraph( op->source(), graph );
+
+        add_edge(v,c,graph);
+    }
+
+    if (childFilter || childSource) {
+        OperationGraph::vertex_descriptor c =
+             (0!=childFilter)
+             ? populateGraph( childFilter, graph )
+             : populateGraph( childSource, graph );
+
+        add_edge(v,c,graph);
+    }
+
+    return v;
+}
+
+template<typename vertex_descriptor>
+bool first_common_vertex_up(const vertex_descriptor& search, const vertex_descriptor& stop, const vertex_descriptor& target, OperationGraph graph)
+{
+    if (search == stop)
+        return false;
+    if (search == target)
+        return true;
+
+    // Find all parents of a
+    pair<OperationGraph::inv_adjacency_iterator, OperationGraph::inv_adjacency_iterator>
+        children = boost::inv_adjacent_vertices(search, graph);
+
+    for (OperationGraph::inv_adjacency_iterator c = children.first; c!=children.second; ++c)
+    {
+        if (first_common_vertex_up(*c, stop, target, graph))
+            return true;
+    }
+
+    return false;
+}
+
+/**
+  Assume that the graph doesn't contain any loops. Whatever is that called now again?
+  Search for 'target' in the graph that can be reached from 'source' by going down and then up.
+  */
+template<typename vertex_descriptor>
+vertex_descriptor first_common_vertex(const vertex_descriptor& source, const vertex_descriptor& target, OperationGraph graph)
+{
+    // Find all childrens of this graph
+    pair<OperationGraph::adjacency_iterator, OperationGraph::adjacency_iterator>
+            children = adjacent_vertices(source, graph);
+
+    for (OperationGraph::adjacency_iterator c = children.first; c!=children.second; ++c)
+    {
+        if (first_common_vertex_up(*c, source, target, graph))
+            return *c;
+        first_common_vertex(*c, target, graph);
+    }
+    return vertex_descriptor_null;
+}
+/*OperationGraph::adjacency_iterator
+template<typename Iterator>
+typename std::iterator_traits<Iterator>::difference_type
+mycount(Iterator first, Iterator second)
+{
+    for ()
+}*/
+
+void updateOperationsTree( OperationGraph::vertex_descriptor v, OperationGraph graph, QTreeWidgetItem* w, OperationGraph::vertex_descriptor stop )
+{
+    BOOST_ASSERT( w );
+
+    QTreeWidgetItem* child = new QTreeWidgetItem( 0 );
+    child->setText(0, QString::fromStdString( graph[v].title ));
+    child->setToolTip(0, QString::fromStdString( graph[v].tooltip ));
     child->setFlags( Qt::ItemIsSelectable | Qt::ItemIsEnabled );
     w->addChild( child );
 
-    if ( Signal::Operation* op = dynamic_cast<Signal::Operation*>(s.get())) {
-        return updateOperationsTree( op->source(), w);
+    // Find all childrens of this graph
+    pair<OperationGraph::adjacency_iterator, OperationGraph::adjacency_iterator>
+            children = adjacent_vertices(v, graph);
+
+    // Count them
+    // size_t numChildren = std::count( children.first, children.second );
+    size_t numChildren = children.second - children.first;
+
+    // If only one, do nothing special
+    if (1==numChildren) {
+        if (*children.first != stop)
+            updateOperationsTree( *children.first, graph, w, stop );
+        return;
     }
+
+    // If more than one however:
+    //  1. count number of disconnected subgraphcs (children usually converge to have some
+    //     common node further down)
+    //  2. for each subgraph
+    //  3. create a subtree with the title subgraph (the first subgraph is promoted to stay
+    //     on the same level with 'w' as parent)
+    //  4. do 1 again
+    //
+    //  1 search all paths and look for the first common node (first connection point)
+    //  2 if a child is found who is not connected  doesn't have .... blaj
+    boost::unordered_set<OperationGraph::vertex_descriptor> commons;
+    for (OperationGraph::adjacency_iterator c = children.first; c!=children.second; c++)
+    {
+        if (*c == stop) continue;
+
+        OperationGraph::adjacency_iterator d = c;
+        d++;
+        for (; d!=children.second; d++)
+        {
+            if (*d == stop) continue;
+            OperationGraph::vertex_descriptor common = first_common_vertex( *c, *d, graph );
+            commons.insert( common );
+        }
+    }
+
+    boost::unordered_set<OperationGraph::vertex_descriptor>::iterator itr;
+    for (OperationGraph::adjacency_iterator c = children.first; c!=children.second; c++)
+    {
+        if (*c == stop) continue;
+
+        OperationGraph::vertex_descriptor stop_vertex = vertex_descriptor_null;
+        for (itr=commons.begin(); itr!=commons.end(); itr++ )
+        {
+            OperationGraph::vertex_descriptor common = first_common_vertex( *c, *itr, graph );
+            if (common != vertex_descriptor_null)
+                stop_vertex = common;
+        }
+
+        if (c == children.first) {
+            updateOperationsTree( *c, graph, w, stop_vertex );
+        } else {
+            updateOperationsTree( *c, graph, child, stop_vertex );
+        }
+    }
+}
+
+void MainWindow::updateOperationsTree( Signal::pSource s )
+{
+    TaskTimer tt("Updating operations tree");
+
+    OperationGraph graph;
+
+    OperationGraph::vertex_descriptor head = populateGraph( s, graph );
+
+    ui->operationsTree->clear();
+
+    QTreeWidgetItem* w = ui->operationsTree->invisibleRootItem();
+    ::updateOperationsTree( head, graph, w, vertex_descriptor_null );
 }
 
 void MainWindow::updateLayerList( Tfr::pFilter f )
