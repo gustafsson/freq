8c9a0705c6af9933afb42bad52b538f2e57d69d9
Johan <johan.b.gustafsson@gmail.com>
2010-05-13 03:04:43 -0700

    mark-up formatting

--- use-cases/Enhance a frequency range in a certain passage.mk
+++ use-cases/Enhance a frequency range in a certain passage.mk
@@ -1,5 +1,5 @@
-Business case: Audio editing suite
+#Business case: Audio editing suite
 
-1 User wants to boost the bass / mid / treble in a passage for more OOMPH!
-2 Locate the passage using the time line / overview / ears
-3 Using large size brushes to raise / lower portions of the scalogram
+* User wants to boost the bass / mid / treble in a passage for more OOMPH!
+* Locate the passage using the time line / overview / ears
+* Using large size brushes to raise / lower portions of the scalogram
