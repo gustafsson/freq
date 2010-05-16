0eafade8b638b7f32df1176926f0adcf95de58aa
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2010-05-16 18:27:10 +0200

    Produces a correct graph from nested operations but renders the graph with a few errors in tree form.

--- displaywidget.cpp
+++ displaywidget.cpp
@@ -321,6 +321,7 @@ void DisplayWidget::receiveAddClearSelection(bool active)
     getFilterOperation()->filter()->enabled = true;
 
     setWorkerSource();
+    update();
 }
 
 void DisplayWidget::setWorkerSource( Signal::pSource s ) {
@@ -348,8 +349,9 @@ void DisplayWidget::receiveAddSelection(bool /*active*/)
             (unsigned)(start*f->sample_rate()),
             (unsigned)(end*f->sample_rate()));
     _renderer->collection()->updateInvalidSamples(sid);
-    update();
+
     setWorkerSource();
+    update();
 }
 
 void DisplayWidget::
@@ -372,10 +374,12 @@ void DisplayWidget::
     // Invalidate rendering
     Signal::SamplesIntervalDescriptor sid(start, b->number_of_samples());
     _renderer->collection()->updateInvalidSamples(sid);
-    update();
 
     // Update stream
     b->source(remove);
+
+    setWorkerSource();
+    update();
 }
 
 void DisplayWidget::
@@ -422,10 +426,12 @@ void DisplayWidget::
         Signal::SamplesIntervalDescriptor sid(oldStart, oldStart+L);
         sid |= Signal::SamplesIntervalDescriptor(newStart, newStart+L);
         _renderer->collection()->updateInvalidSamples(sid);
-        update();
 
         // update stream
         b->source(mergeSelection);
+
+        setWorkerSource();
+        update();
     }
 }
 
@@ -462,10 +468,12 @@ void DisplayWidget::
         Signal::SamplesIntervalDescriptor sid(oldStart, oldStart+L);
         sid |= Signal::SamplesIntervalDescriptor(newStart, newStart+L);
         _renderer->collection()->updateInvalidSamples(sid);
-        update();
 
         // update stream
         b->source(moveSelection );
+
+        setWorkerSource();
+        update();
     }
 }
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -120,6 +120,7 @@ void MainWindow::connectLayerWindow(DisplayWidget *d)
 
 struct TitleAndTooltip {
     std::string title, tooltip;
+    void* ptrData;
 };
 
 typedef boost::adjacency_list<vecS,vecS, bidirectionalS, TitleAndTooltip> OperationGraph;
@@ -127,6 +128,13 @@ const OperationGraph::vertex_descriptor vertex_descriptor_null = (OperationGraph
 
 OperationGraph::vertex_descriptor populateGraph( Tfr::pFilter f, OperationGraph& graph )
 {
+    // Search for vertex in graph
+    OperationGraph::vertex_iterator i, end;
+    for (tie(i,end) = vertices( graph ); i!=end; ++i) {
+        if (f.get() == graph[*i].ptrData)
+            return *i;
+    }
+
     stringstream title;
     stringstream tooltip;
     title << fixed << setprecision(1);
@@ -160,8 +168,11 @@ OperationGraph::vertex_descriptor populateGraph( Tfr::pFilter f, OperationGraph&
     TitleAndTooltip tat;
     tat.title = title.str();
     tat.tooltip = tooltip.str();
+    tat.ptrData = f.get();
 
     OperationGraph::vertex_descriptor v = add_vertex(tat, graph);
+
+    TaskTimer tt("%d %s", (unsigned)v, tat.title.c_str()); tt.suppressTiming();
 
     if (filter_chain) {
         BOOST_FOREACH( Tfr::pFilter f, *filter_chain ) {
@@ -169,6 +180,7 @@ OperationGraph::vertex_descriptor populateGraph( Tfr::pFilter f, OperationGraph&
             c = populateGraph( f, graph );
 
             add_edge(v,c,graph);
+            tt.info("%d -> %d", v,c);
         }
     }
 
@@ -177,6 +189,13 @@ OperationGraph::vertex_descriptor populateGraph( Tfr::pFilter f, OperationGraph&
 
 OperationGraph::vertex_descriptor populateGraph( Signal::pSource s, OperationGraph& graph )
 {
+    // Search for vertex in graph
+    OperationGraph::vertex_iterator i, end;
+    for (tie(i,end) = vertices( graph ); i!=end; ++i) {
+        if (s.get() == graph[*i].ptrData)
+            return *i;
+    }
+
     stringstream title;
     stringstream tooltip;
     title << fixed << setprecision(1);
@@ -224,14 +243,18 @@ OperationGraph::vertex_descriptor populateGraph( Signal::pSource s, OperationGra
     TitleAndTooltip tat;
     tat.title = title.str();
     tat.tooltip = tooltip.str();
+    tat.ptrData = s.get();
 
     OperationGraph::vertex_descriptor v = add_vertex(tat, graph);
+
+    TaskTimer tt("%d %s", (unsigned)v, tat.title.c_str()); tt.suppressTiming();
 
     if ( Signal::Operation* op = dynamic_cast<Signal::Operation*>(s.get())) {
         OperationGraph::vertex_descriptor c;
         c = populateGraph( op->source(), graph );
 
         add_edge(v,c,graph);
+        tt.info("%d -> %d", v,c);
     }
 
     if (childFilter || childSource) {
@@ -241,13 +264,14 @@ OperationGraph::vertex_descriptor populateGraph( Signal::pSource s, OperationGra
              : populateGraph( childSource, graph );
 
         add_edge(v,c,graph);
+        tt.info("%d -> %d", v,c);
     }
 
     return v;
 }
 
 template<typename vertex_descriptor>
-bool first_common_vertex_up(const vertex_descriptor& search, const vertex_descriptor& stop, const vertex_descriptor& target, OperationGraph graph)
+bool first_common_vertex_up(const vertex_descriptor& search, const vertex_descriptor& target, const vertex_descriptor& stop, OperationGraph graph)
 {
     if (search == stop)
         return false;
@@ -256,11 +280,11 @@ bool first_common_vertex_up(const vertex_descriptor& search, const vertex_descri
 
     // Find all parents of a
     pair<OperationGraph::inv_adjacency_iterator, OperationGraph::inv_adjacency_iterator>
-        children = boost::inv_adjacent_vertices(search, graph);
+        parents = boost::inv_adjacent_vertices(search, graph);
 
-    for (OperationGraph::inv_adjacency_iterator c = children.first; c!=children.second; ++c)
+    for (OperationGraph::inv_adjacency_iterator c = parents.first; c!=parents.second; ++c)
     {
-        if (first_common_vertex_up(*c, stop, target, graph))
+        if (first_common_vertex_up(*c, target, stop, graph))
             return true;
     }
 
@@ -272,45 +296,46 @@ bool first_common_vertex_up(const vertex_descriptor& search, const vertex_descri
   Search for 'target' in the graph that can be reached from 'source' by going down and then up.
   */
 template<typename vertex_descriptor>
-vertex_descriptor first_common_vertex(const vertex_descriptor& source, const vertex_descriptor& target, OperationGraph graph)
+vertex_descriptor first_common_vertex(const vertex_descriptor& source, const vertex_descriptor& target, const vertex_descriptor& stop, OperationGraph graph)
 {
+    if (first_common_vertex_up(source, target, stop, graph))
+        return source;
+
     // Find all childrens of this graph
     pair<OperationGraph::adjacency_iterator, OperationGraph::adjacency_iterator>
             children = adjacent_vertices(source, graph);
 
     for (OperationGraph::adjacency_iterator c = children.first; c!=children.second; ++c)
     {
-        if (first_common_vertex_up(*c, source, target, graph))
-            return *c;
-        first_common_vertex(*c, target, graph);
+        vertex_descriptor v = first_common_vertex(*c, target, stop, graph);
+        if (vertex_descriptor_null != v)
+            return v;
+
     }
     return vertex_descriptor_null;
 }
-/*OperationGraph::adjacency_iterator
-template<typename Iterator>
-typename std::iterator_traits<Iterator>::difference_type
-mycount(Iterator first, Iterator second)
-{
-    for ()
-}*/
 
-void updateOperationsTree( OperationGraph::vertex_descriptor v, OperationGraph graph, QTreeWidgetItem* w, OperationGraph::vertex_descriptor stop )
+void updateOperationsTree( OperationGraph::vertex_descriptor v, OperationGraph& graph, QTreeWidgetItem* w, OperationGraph::vertex_descriptor stop )
 {
     BOOST_ASSERT( w );
 
+    const TitleAndTooltip& tat = graph[v];
     QTreeWidgetItem* child = new QTreeWidgetItem( 0 );
-    child->setText(0, QString::fromStdString( graph[v].title ));
-    child->setToolTip(0, QString::fromStdString( graph[v].tooltip ));
+    child->setText(0, QString::fromStdString( tat.title ));
+    child->setToolTip(0, QString::fromStdString( tat.tooltip ));
     child->setFlags( Qt::ItemIsSelectable | Qt::ItemIsEnabled );
     w->addChild( child );
 
-    // Find all childrens of this graph
+    // Find all childrens of this vertex
     pair<OperationGraph::adjacency_iterator, OperationGraph::adjacency_iterator>
             children = adjacent_vertices(v, graph);
 
     // Count them
     // size_t numChildren = std::count( children.first, children.second );
     size_t numChildren = children.second - children.first;
+
+    TaskTimer tt("%d %s (%d children)", (unsigned)v, tat.title.c_str(), numChildren);
+    tt.suppressTiming();
 
     // If only one, do nothing special
     if (1==numChildren) {
@@ -332,16 +357,18 @@ void updateOperationsTree( OperationGraph::vertex_descriptor v, OperationGraph g
     boost::unordered_set<OperationGraph::vertex_descriptor> commons;
     for (OperationGraph::adjacency_iterator c = children.first; c!=children.second; c++)
     {
-        if (*c == stop) continue;
-
         OperationGraph::adjacency_iterator d = c;
         d++;
         for (; d!=children.second; d++)
         {
-            if (*d == stop) continue;
-            OperationGraph::vertex_descriptor common = first_common_vertex( *c, *d, graph );
-            commons.insert( common );
+            OperationGraph::vertex_descriptor common = first_common_vertex( *c, *d, v, graph );
+            if (common != vertex_descriptor_null)
+                commons.insert( common );
         }
+    }
+
+    BOOST_FOREACH( const OperationGraph::vertex_descriptor& common, commons ) {
+        tt.info("Children converge at: %d %s", common, graph[common].title.c_str());
     }
 
     boost::unordered_set<OperationGraph::vertex_descriptor>::iterator itr;
@@ -352,10 +379,17 @@ void updateOperationsTree( OperationGraph::vertex_descriptor v, OperationGraph g
         OperationGraph::vertex_descriptor stop_vertex = vertex_descriptor_null;
         for (itr=commons.begin(); itr!=commons.end(); itr++ )
         {
-            OperationGraph::vertex_descriptor common = first_common_vertex( *c, *itr, graph );
-            if (common != vertex_descriptor_null)
+            OperationGraph::vertex_descriptor common = first_common_vertex( *c, *itr, v, graph );
+            if (common != vertex_descriptor_null) {
                 stop_vertex = common;
+                // TODO it feels like there could be a bug around here... think this through again and explain the procedure
+            }
         }
+
+        if (stop_vertex != vertex_descriptor_null)
+            tt.info("Following children until: %d %s", stop_vertex, graph[stop_vertex].title.c_str());
+        else
+            tt.info("Following children to the end of the graph");
 
         if (c == children.first) {
             updateOperationsTree( *c, graph, w, stop_vertex );
