26579b283db7b5bb658af30a687f8f645e4ef4c5
Ulf Hammarqvist <ulf.hammarqvist@gmail.com>
2010-05-24 17:19:30 +0200

    stuffs

--- sawe-hdf5.cpp
+++ sawe-hdf5.cpp
@@ -19,7 +19,7 @@ string hdf5_number()
     for (unsigned c = 1; c<10; c++)
     {
         stringstream filename;
-        filename << basename << c << ".hdf50";
+        filename << basename << c << ".hdf5";
         fstream hdf5(filename.str().c_str());
         if (!hdf5.is_open())
             return filename.str();
@@ -44,7 +44,7 @@ put( Signal::pBuffer b )
 	hsize_t     dims[RANK]={2,s.width,s.height};
 
 	/* create a HDF5 file */
-	file_id = H5Fcreate ("ex_lite1.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	file_id = H5Fcreate (filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
 	/* create and write an integer type dataset named "dset" */
 	status = H5LTmake_dataset(file_id,"/dset",RANK,dims,H5T_NATIVE_FLOAT,p);
