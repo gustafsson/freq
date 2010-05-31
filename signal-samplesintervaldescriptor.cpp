b24baca1cf68c4148d4c513a36b63586ba3bcfa0
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2010-05-31 18:46:08 +0200

    Added correct handling of a special case in SamplesIntervalDescriptor.

--- signal-samplesintervaldescriptor.cpp
+++ signal-samplesintervaldescriptor.cpp
@@ -11,20 +11,32 @@ namespace Signal {
 const SamplesIntervalDescriptor::SampleType SamplesIntervalDescriptor::SampleType_MIN = (SamplesIntervalDescriptor::SampleType)0;
 const SamplesIntervalDescriptor::SampleType SamplesIntervalDescriptor::SampleType_MAX = (SamplesIntervalDescriptor::SampleType)-1;
 const SamplesIntervalDescriptor SamplesIntervalDescriptor::SamplesIntervalDescriptor_ALL = SamplesIntervalDescriptor(SamplesIntervalDescriptor::SampleType_MIN, SamplesIntervalDescriptor::SampleType_MAX);
+const SamplesIntervalDescriptor::Interval SamplesIntervalDescriptor::SamplesInterval_ALL = { SamplesIntervalDescriptor::SampleType_MIN, SamplesIntervalDescriptor::SampleType_MAX };
+
+bool SamplesIntervalDescriptor::Interval::
+        valid() const
+{
+    return first < last;
+}
+
+bool SamplesIntervalDescriptor::Interval::
+        isConnectedTo(const Interval& r) const
+{
+    return last >= r.first && r.last >= first;
+}
 
 bool SamplesIntervalDescriptor::Interval::
         operator<(const Interval& r) const
 {
-    return last < r.first;
+    return first < r.first;
 }
 
-bool SamplesIntervalDescriptor::Interval::
+SamplesIntervalDescriptor::Interval& SamplesIntervalDescriptor::Interval::
         operator|=(const Interval& r)
 {
-    bool b = (*this < r) == (r < *this);
     first = std::min(first, r.first);
     last = std::max(last, r.last);
-    return b;
+    return *this;
 }
 
 bool SamplesIntervalDescriptor::Interval::
@@ -67,19 +79,23 @@ SamplesIntervalDescriptor& SamplesIntervalDescriptor::
     _intervals.push_back( r );
     _intervals.sort();
 
-    for (std::list<Interval>::iterator itr = _intervals.begin(); itr!=_intervals.end(); itr++) {
+    for (std::list<Interval>::iterator itr = _intervals.begin(); itr!=_intervals.end(); ) {
         std::list<Interval>::iterator next = itr;
         next++;
-        if (next==_intervals.end())
-            continue;
+        if (next!=_intervals.end()) {
+            Interval& a = *itr;
+            Interval& b = *next;
 
-        Interval& a = *itr;
-        Interval& b = *next;
-
-        if ((a<b) == (b<a))
-        {
-            a |= b;
-            _intervals.erase( next );
+            if (a.isConnectedTo(b))
+            {
+                a |= b;
+                itr = _intervals.erase( next );
+                itr--;
+            } else {
+                itr++;
+            }
+        } else {
+            itr++;
         }
     }
     return *this;
@@ -99,7 +115,7 @@ SamplesIntervalDescriptor& SamplesIntervalDescriptor::
     for (std::list<Interval>::iterator itr = _intervals.begin(); itr!=_intervals.end();) {
         Interval& i = *itr;
         // Check if interval 'itr' intersects with 'r'
-        if (!(i<r) && !(r<i)) {
+        if (i.isConnectedTo(r)) {
 
             // Check if intersection is over the start of 'itr'
             if (i.first >= r.first && i.last > r.last) {
@@ -127,6 +143,7 @@ SamplesIntervalDescriptor& SamplesIntervalDescriptor::
 
             // Else, error
             } else {
+                BOOST_ASSERT( false );
                 throw std::logic_error("Shouldn't reach here");
             }
         } else {
@@ -201,13 +218,11 @@ SamplesIntervalDescriptor& SamplesIntervalDescriptor::
         Interval& i = *itr;
 
         // Check if interval 'itr' does not intersect with 'r'
-        if ((i.last<=r.first) != (r.last<=i.first)) {
+        if ((i.last<=r.first) || (r.last<=i.first)) {
             itr = _intervals.erase(itr);
 
-        } else if ((i<r) && (r<i)) {
-            throw std::logic_error("Shouldn't reach here");
-
         } else {
+            BOOST_ASSERT(i.isConnectedTo(r));
 
             // Check if intersection is over the start of 'itr'
             if (i.first >= r.first && i.last > r.last)
@@ -252,7 +267,7 @@ SamplesIntervalDescriptor::Interval SamplesIntervalDescriptor::
         getInterval( SampleType dt, SampleType center ) const
 {
     if (0 == _intervals.size()) {
-        Interval r = {0.f, 0.f};
+        Interval r = {SampleType_MIN, SampleType_MIN};
         return r;
     }
 
@@ -303,26 +318,58 @@ SamplesIntervalDescriptor::Interval SamplesIntervalDescriptor::
     }
 }
 
-void SamplesIntervalDescriptor::
-        print( std::string title )
+SamplesIntervalDescriptor::Interval SamplesIntervalDescriptor::
+        getInterval( Interval n ) const
 {
-    //TaskTimer tt("%s, %d intervals", title.empty()?"SamplesIntervalDescriptor":title.c_str(), _intervals.size());
+    SamplesIntervalDescriptor sid = *this & n;
 
-    //tt.suppressTiming();
-    //tt.attemptOneRow();
-
-    std::stringstream ss;
-    BOOST_FOREACH (const Interval& r, _intervals) {
-        ss << " [" << r.first << ", " << r.last << "]";
-        //tt.info("[%u, %u]", r.first, r.last );
+    if (0 == sid._intervals.size()) {
+        Interval r = {SampleType_MIN, SampleType_MIN};
+        return r;
     }
-    ss << '.';
 
-    TaskTimer("%s, %d interval%s%s",
+    return sid.intervals().front();
+}
+
+SamplesIntervalDescriptor::Interval SamplesIntervalDescriptor::
+        coveredInterval() const
+{
+    Interval i;
+    if (_intervals.empty()) {
+        i.first = i.last = 0;
+        return i;
+    }
+
+    i.first = _intervals.front().first;
+    i.last = _intervals.back().last;
+
+    return i;
+}
+
+void SamplesIntervalDescriptor::
+        print( std::string title ) const
+{
+    std::stringstream ss;
+    ss << *this;
+
+    TaskTimer("%s, %s",
               title.empty()?"SamplesIntervalDescriptor":title.c_str(),
-              _intervals.size(),
-              _intervals.size()==1?"":"s",
               ss.str().c_str()).suppressTiming();
+}
+
+std::ostream& operator<<( std::ostream& s, const SamplesIntervalDescriptor& i)
+{
+    s << "{" << i.intervals().size() << " interval" << ((i.intervals().size()==1)?"":"s");
+
+    BOOST_FOREACH (const SamplesIntervalDescriptor::Interval& r, i.intervals())
+        s << " " << r;
+
+    return s << "}";
+}
+
+std::ostream& operator<<( std::ostream& s, const SamplesIntervalDescriptor::Interval& i)
+{
+    return s << "[" << i.first << ", " << i.last << "]";
 }
 
 } // namespace Signal
--- signal-samplesintervaldescriptor.h
+++ signal-samplesintervaldescriptor.h
@@ -2,7 +2,7 @@
 #define SAMPLESINTERVALDESCRIPTOR_H
 
 #include <list>
-#include <stdexcept> // TODO remove temp code
+#include <ostream>
 
 namespace Signal {
 
@@ -10,9 +10,6 @@ class SamplesIntervalDescriptor
 {
 public:
     typedef unsigned SampleType;
-    static const SampleType SampleType_MIN;
-    static const SampleType SampleType_MAX;
-    static const SamplesIntervalDescriptor SamplesIntervalDescriptor_ALL;
 
     struct Interval {
         /**
@@ -21,37 +18,48 @@ public:
           */
         SampleType first, last;
 
+        bool valid() const;
+        bool isConnectedTo(const Interval& r) const;
         bool operator<(const Interval& r) const;
-        bool operator|=(const Interval& r);
+        Interval& operator|=(const Interval& r);
         bool operator==(const Interval& r) const;
     };
 
+    static const SampleType SampleType_MIN;
+    static const SampleType SampleType_MAX;
+    static const SamplesIntervalDescriptor SamplesIntervalDescriptor_ALL;
+    static const Interval SamplesInterval_ALL;
+
     SamplesIntervalDescriptor( );
     SamplesIntervalDescriptor( Interval );
-    SamplesIntervalDescriptor( float , float )
-    {
-        throw std::runtime_error("NOOO2");
-    }
     SamplesIntervalDescriptor( SampleType first, SampleType last );
+    SamplesIntervalDescriptor operator | (const SamplesIntervalDescriptor& b) const { SamplesIntervalDescriptor a = *this; return a|=b; }
     SamplesIntervalDescriptor& operator |= (const SamplesIntervalDescriptor&);
     SamplesIntervalDescriptor& operator |= (const Interval&);
+    SamplesIntervalDescriptor operator - (const SamplesIntervalDescriptor& b) const { SamplesIntervalDescriptor a = *this; return a-=b; }
     SamplesIntervalDescriptor& operator -= (const SamplesIntervalDescriptor&);
     SamplesIntervalDescriptor& operator -= (const Interval&);
     SamplesIntervalDescriptor& operator -= (const SampleType&);
     SamplesIntervalDescriptor& operator += (const SampleType&);
+    SamplesIntervalDescriptor operator & (const SamplesIntervalDescriptor& b) const { SamplesIntervalDescriptor a = *this; return a&=b; }
     SamplesIntervalDescriptor& operator &= (const SamplesIntervalDescriptor&);
     SamplesIntervalDescriptor& operator &= (const Interval&);
     SamplesIntervalDescriptor& operator *= (const float& scale);
 
-    Interval    getInterval( SampleType dt, SampleType center = 0 ) const;
+    bool                            isEmpty() const { return _intervals.empty(); }
+    Interval                        getInterval( SampleType dt, SampleType center = SampleType_MIN ) const;
+    Interval                        getInterval( Interval n = SamplesInterval_ALL ) const;
+    Interval                        coveredInterval() const;
+    const std::list<Interval>&      intervals() const { return _intervals; }
 
-    bool isEmpty() const { return _intervals.empty(); }
-    const std::list<Interval>& intervals() { return _intervals; }
-    void print( std::string title="" );
+    void                            print( std::string title="" ) const;
 
 private:
     std::list<Interval> _intervals;
 };
+
+std::ostream& operator<<( std::ostream& s, const SamplesIntervalDescriptor& i);
+std::ostream& operator<<( std::ostream& s, const SamplesIntervalDescriptor::Interval& i);
 
 } // namespace Signal
 
