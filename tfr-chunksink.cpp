5c257c6921e0032175db5ca5a4ad5fe6a5c197b4
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2010-06-17 09:11:21 +0200

    Export sample_rate in chunks to matlab.

--- tfr-chunksink.cpp
+++ tfr-chunksink.cpp
@@ -60,10 +60,11 @@ pChunk ChunkSink::
     clamped->transform_data.reset( new GpuCpuData<float2>(0, make_cudaExtent( c->n_valid_samples, c->nScales(), c->nChannels() )));
 
     ::wtClamp( c->transform_data->getCudaGlobal(), c->first_valid_sample, clamped->transform_data->getCudaGlobal() );
-    clamped->chunk_offset = c->chunk_offset + c->first_valid_sample;
-    clamped->first_valid_sample = 0;
     clamped->max_hz = c->max_hz;
     clamped->min_hz = c->min_hz;
+    clamped->chunk_offset = c->chunk_offset + c->first_valid_sample;
+	clamped->sample_rate = c->sample_rate;
+    clamped->first_valid_sample = 0;
     clamped->n_valid_samples = c->n_valid_samples;
 
     return clamped;
