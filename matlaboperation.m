db74d1a1c4c72d204fd238c1643180fb2e9f29a7
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2010-06-25 11:26:51 +0200

    Clarified matlab operation example

--- matlaboperation.m
+++ matlaboperation.m
@@ -2,7 +2,10 @@ function [data,dummy]=matlaboperation(data,dummy)
 FS = data.samplerate(1); % data.samplerate is a matrix, not a scalar
 offset = data.offset(1);
 
+lowpass=0.05;
+
 F=fft(data.buffer);
-F(round(end/20):round(19*end/20))=0;
+nsave=round(lowpass*size(F,1));
+F(1+nsave:end-nsave)=0;
 data.buffer=real(ifft(F));
 %endfunction % octave
