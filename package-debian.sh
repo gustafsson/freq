c0a9a57085210d98cf9c67d8a773596e371a8b31
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2010-06-25 21:28:37 +0200

    Prepared new linux release with matlab examples.

--- package-debian.sh
+++ package-debian.sh
@@ -2,6 +2,21 @@
 rm -f package-debian/DEBIAN/*~ && \
 cp sonicawe package-debian/usr/bin && \
 cp sonicawe.1 package-debian/usr/share/man/man1 && \
+cp sawe_extract_cwt.m package-debian/usr/share/sonicawe && \
+cp sawe_extract_cwt_time.m package-debian/usr/share/sonicawe && \
+cp sawe_filewatcher.m package-debian/usr/share/sonicawe && \
+cp sawe_filewatcher_oct.m package-debian/usr/share/sonicawe && \
+cp matlabfilter.m package-debian/usr/share/sonicawe && \
+cp matlaboperation.m package-debian/usr/share/sonicawe && \
+cp sawe_loadbuffer.m package-debian/usr/share/sonicawe && \
+cp sawe_loadbuffer_oct.m package-debian/usr/share/sonicawe && \
+cp sawe_loadchunk.m package-debian/usr/share/sonicawe && \
+cp sawe_loadchunk_oct.m package-debian/usr/share/sonicawe && \
+cp sawe_savebuffer.m package-debian/usr/share/sonicawe && \
+cp sawe_savebuffer_oct.m package-debian/usr/share/sonicawe && \
+cp sawe_savechunk.m package-debian/usr/share/sonicawe && \
+cp sawe_savechunk_oct.m package-debian/usr/share/sonicawe && \
+cp credits.txt package-debian/usr/share/sonicawe && \
 pushd package-debian && \
 gzip -f usr/share/man/man1/sonicawe.1 && \
 rm -f DEBIAN/md5sums && \
