29a26c9e4eb3240c445fff5ba3223b2f19dcbc74
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2010-08-14 19:01:09 +0200

    Optimerade wavelet ridge

--- heightmap-collection.cpp
+++ heightmap-collection.cpp
@@ -80,31 +80,47 @@ void Collection::
     }
 
     switch(_transformMethod){
-        case TransformMethod_Cwt_ridge:{
-            float2* p = chunk->transform_data->getCpuMemory();
-            for (unsigned x=0; x<chunk->nSamples(); x++) {
+        case TransformMethod_Cwt_ridge:
+        {
+            float2* p     = chunk->transform_data->getCpuMemory();
 
-                std::vector<float2> q(chunk->nScales());
-                for (unsigned y=0; y<chunk->nScales(); y++)
-                    q[y] = make_float2(0,0);
+            std::vector<float2> q[] = {
+                std::vector<float2>(chunk->nSamples()),
+                std::vector<float2>(chunk->nSamples())
+            };
 
-                for (unsigned y=1; y<chunk->nScales()-1; y++)
+            unsigned bytes_per_row = sizeof(float2)*chunk->nSamples();
+
+            float2
+                    *prev_row = &q[0][0],
+                    *this_row = &q[1][0];
+
+            for (unsigned y=1; y<chunk->nScales()-1; y++)
+            {
+                float2* t = prev_row;
+                prev_row = this_row;
+                this_row = t;
+
+                for (unsigned x=0; x<chunk->nSamples(); x++)
                 {
                     float2 a = p[(y-1)*chunk->nSamples() + x];
                     float2 b = p[(y+0)*chunk->nSamples() + x];
                     float2 c = p[(y+1)*chunk->nSamples() + x];
-                    if (b.x*b.x+b.y*b.y > a.x*a.x+a.y*a.y && b.x*b.x+b.y*b.y>c.x*c.x+c.y*c.y)
-                         q[y] = b;
+                    float A = a.x*a.x + a.y*a.y;
+                    float B = b.x*b.x + b.y*b.y;
+                    float C = c.x*c.x + c.y*c.y;
+                    this_row[x] = (B > A  &&  B > C) ? b : make_float2(0,0);
                 }
 
-                for (unsigned y=0; y<chunk->nScales(); y++)
-                    p[y*chunk->nSamples() + x] = q[y];
+                if (1<y)
+                    memcpy( p + (y-1)*chunk->nSamples(), prev_row, bytes_per_row );
             }
+            memcpy( p + (chunk->nScales()-2)*chunk->nSamples(),
+                    this_row,
+                    bytes_per_row );
+            memset( p + (chunk->nScales()-1)*chunk->nSamples(), 0, bytes_per_row );
+            memset( p                                         , 0, bytes_per_row );
 
-            for (unsigned x=0; x<chunk->nSamples(); x++) {
-                p[(chunk->nScales()-1)*chunk->nSamples() + x] = make_float2(0,0);
-                p[x] = make_float2(0,0);
-            }
             break;
         }
        case TransformMethod_Cwt_reassign: {
