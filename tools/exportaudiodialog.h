842ef8962952ac153b085c607477bba245e96892
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-02-07 17:47:03 +0100

    Export audio dialog

---
+++ tools/exportaudiodialog.h
@@ -0,0 +1,57 @@
+#ifndef EXPORTAUDIODIALOG_H
+#define EXPORTAUDIODIALOG_H
+
+#include <QDialog>
+#include <QTimer>
+
+#include "sawe/project.h"
+
+#include "selectionmodel.h"
+
+namespace Tools {
+
+namespace Ui {
+    class ExportAudioDialog;
+}
+
+class ExportAudioDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ExportAudioDialog(
+            Sawe::Project* project,
+            SelectionModel* selection_model,
+            RenderView* render_view );
+    ~ExportAudioDialog();
+
+public slots:
+    void exportEntireFile();
+    void exportSelection();
+    void abortExport();
+    void selectionChanged();
+    void populateTodoList();
+
+private:
+    Ui::ExportAudioDialog *ui;
+
+    Sawe::Project* project;
+    SelectionModel* selection_model;
+    RenderView* render_view;
+
+    Signal::pWorkerCallback worker_callback;
+    QTimer update_timer;
+    QString filemame;
+    Signal::IntervalType total;
+
+    // overloaded from QWidget
+    virtual void paintEvent ( QPaintEvent * event );
+
+    void setupGui();
+
+    void start(Signal::pOperation filter);
+};
+
+
+} // namespace Tools
+#endif // EXPORTAUDIODIALOG_H
