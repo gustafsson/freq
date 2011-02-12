742a64c3ab4e201d34b37a9aa14874c27b561ac8
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-02-12 17:01:27 +0100

    Using set -e in scripts

--- dist/deploy-debian.sh
+++ dist/deploy-debian.sh
@@ -1,28 +1,37 @@
-read -s -p "Enter password for ftp.sonicawe.com: " pass && echo && \
-if [ -z "$pass" ]; then echo "Missing password for ftp.sonicawe.com, can't deploy."; exit; fi && \
-cd ../../gpumisc && \
-if [ -n "$(git status -uno --porcelain)" ]; then echo "In gpumisc: local git repo is not clean."; exit; fi && \
-cd ../sonicawe && \
-if [ -n "$(git status -uno --porcelain)" ]; then echo "In sonicawe: local git repo is not clean."; exit; fi && \
-version=$(date +0.%Y.%m.%d) && \
-filename="sonicawe_${version}_snapshot_x86_64.deb" && \
-branch=$(git rev-parse --abbrev-ref HEAD) && \
-git pull --rebase origin $branch && \
-vim dist/package-debian/DEBIAN/control && \
-cd ../gpumisc && \
-git pull --rebase origin master && \
-cd .. && \
-make distclean && \
-qmake && \
-make -j5 && \
-cd sonicawe/dist && \
-./package-debian.sh ${version}_snapshot && \
+#!/bin/bash
+set -e
+
+read -s -p "Enter password for ftp.sonicawe.com: " pass; echo
+if [ -z "$pass" ]; then echo "Missing password for ftp.sonicawe.com, can't deploy."; exit; fi
+cd ../../gpumisc
+if [ -n "$(git status -uno --porcelain)" ]; then echo "In gpumisc: local git repo is not clean."; exit; fi
+cd ../sonicawe
+if [ -n "$(git status -uno --porcelain)" ]; then echo "In sonicawe: local git repo is not clean."; exit; fi
+
+version=$(date +0.%Y.%m.%d)
+filename="sonicawe_${version}_snapshot_x86_64.deb"
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+git pull --rebase origin $branch
+vim dist/package-debian/DEBIAN/control
+
+cd ../gpumisc
+git pull --rebase origin master
+cd ..
+
+make distclean
+qmake
+make -j5
+
+cd sonicawe/dist
+./package-debian.sh ${version}_snapshot
+
 echo "user sonicawe.com $pass
 cd data
 mkdir $version
 cd $version
 passive
-put $filename" | ftp -n -v ftp.sonicawe.com > ftplog.log && \
-echo "Uploaded file to:" && \
+put $filename" | ftp -n -v ftp.sonicawe.com > ftplog.log
+echo "Uploaded file to:"
 echo "http://data.sonicawe.com/${version}/${filename}"
 
--- dist/package-debian.sh
+++ dist/package-debian.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 bury_copy() { mkdir -p "`dirname $2`" && cp "$1" "$2"; }
 
 if [ "$1" ] && [ -z "$2" ] && [ "$(basename `pwd`)" == "dist" ] ; then
@@ -29,40 +31,40 @@ fi
 package=dist/package-debian~
 share=$package/usr/share/sonicawe/.
 
-pushd .. && \
-rm -rf $package && \
-cp -r dist/package-debian $package && \
-mkdir -p $package/usr/lib && \
-mkdir -p $package/usr/bin && \
-cp -r /usr/local/cuda/lib64/libcudart.so* $package/usr/lib/. && \
-cp -r /usr/local/cuda/lib64/libcufft.so* $package/usr/lib/. && \
-cp sonicawe $package/usr/bin/. && \
-bury_copy sonicawe.1 $package/usr/local/share/man/man1/. && \
-mkdir -p $share && \
-cp matlab/sawe_extract_cwt.m $share && \
-cp matlab/sawe_extract_cwt_time.m $share && \
-cp matlab/sawe_filewatcher.m $share && \
-cp matlab/sawe_filewatcher_oct.m $share && \
-cp matlab/matlabfilter.m $share && \
-cp matlab/matlaboperation.m $share && \
-cp matlab/sawe_loadbuffer.m $share && \
-cp matlab/sawe_loadbuffer_oct.m $share && \
-cp matlab/sawe_loadchunk.m $share && \
-cp matlab/sawe_loadchunk_oct.m $share && \
-cp matlab/sawe_savebuffer.m $share && \
-cp matlab/sawe_savebuffer_oct.m $share && \
-cp matlab/sawe_savechunk.m $share && \
-cp matlab/sawe_savechunk_oct.m $share && \
-cp -r license $share && \
-pushd $package && \
-gzip -f usr/local/share/man/man1/sonicawe.1 && \
-rm -f DEBIAN/md5sums && \
-for i in `find -name *~`; do rm $i; done && \
-for i in `find usr -type f`; do md5sum $i >> DEBIAN/md5sums; done && \
-for i in `find usr -type l`; do md5sum $i >> DEBIAN/md5sums; done && \
-popd && \
-output_deb="sonicawe_"$version"_`uname -m`.deb" && \
-dpkg -b $package dist/$output_deb && \
-echo "OUTPUT" && \
-echo "    `pwd`/dist/$output_deb" && \
+pushd ..
+rm -rf $package
+cp -r dist/package-debian $package
+mkdir -p $package/usr/lib
+mkdir -p $package/usr/bin
+cp -r /usr/local/cuda/lib64/libcudart.so* $package/usr/lib/.
+cp -r /usr/local/cuda/lib64/libcufft.so* $package/usr/lib/.
+cp sonicawe $package/usr/bin/.
+bury_copy sonicawe.1 $package/usr/local/share/man/man1/.
+mkdir -p $share
+cp matlab/sawe_extract_cwt.m $share
+cp matlab/sawe_extract_cwt_time.m $share
+cp matlab/sawe_filewatcher.m $share
+cp matlab/sawe_filewatcher_oct.m $share
+cp matlab/matlabfilter.m $share
+cp matlab/matlaboperation.m $share
+cp matlab/sawe_loadbuffer.m $share
+cp matlab/sawe_loadbuffer_oct.m $share
+cp matlab/sawe_loadchunk.m $share
+cp matlab/sawe_loadchunk_oct.m $share
+cp matlab/sawe_savebuffer.m $share
+cp matlab/sawe_savebuffer_oct.m $share
+cp matlab/sawe_savechunk.m $share
+cp matlab/sawe_savechunk_oct.m $share
+cp -r license $share
+pushd $package
+gzip -f usr/local/share/man/man1/sonicawe.1
+rm -f DEBIAN/md5sums
+for i in `find -name *~`; do rm $i; done
+for i in `find usr -type f`; do md5sum $i >> DEBIAN/md5sums; done
+for i in `find usr -type l`; do md5sum $i >> DEBIAN/md5sums; done
+popd
+output_deb="sonicawe_"$version"_`uname -m`.deb"
+dpkg -b $package dist/$output_deb
+echo "OUTPUT"
+echo "    `pwd`/dist/$output_deb"
 popd
