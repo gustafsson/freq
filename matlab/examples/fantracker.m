fa72f3e80ea4926f2bcb177dbac73fa5c9910a6f
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-03-15 18:32:04 +0100

    Fantracker images were taken with this script

--- matlab/examples/fantracker.m
+++ matlab/examples/fantracker.m
@@ -1,10 +1,26 @@
-function [data]=fantracker(data, windowsize)
+function [data]=fantracker(data, args)
 
-if nargin==1 || isempty(windowsize)
-    windowsize = 2^11;
+windowsize = 2^11;
+max_hz = 70;
+min_hz = 50;
+channelnumber = 1:size(data.buffer,2);
+
+if nargin==2 && ~isempty(args)
+  windowsize = args(1);
+  if (numel(args)>1);  min_hz = args(2); end
+  if (numel(args)>2);  max_hz = args(3); end
+  if (numel(args)>3);  channelnumber = args(4); end
 end
 
-disp( ['fantracker ' getdatainfo(data) ' windowsize=' num2str(windowsize)] );
+min_hz = max(min_hz, 2*data.samplerate/windowsize);
+
+disp( ['fantracker ' getdatainfo(data) ' windowsize=' num2str(windowsize) ' hz=[' num2str(min_hz) ', ' num2str(max_hz) ']' ] );
+
+global state;
+if data.offset==0
+  state.hz = [];
+  state.e = 0.3;
+end
 
 % Make sure data is aligned to windowsize
 start = mod(windowsize - mod(data.offset,windowsize), windowsize);
@@ -21,19 +37,16 @@ end
 
 data.redundancy = 0;
 
-for channel=1:size(data.buffer,2)
+for channel=channelnumber
   signal = data.buffer(:, channel);
 
   [ft, dummy] = stft(signal, windowsize, windowsize, windowsize/2, 3);
   C = log(4 + abs(ft));
   [C, dummy] = stft(C(:), windowsize, windowsize, windowsize/2, 3);
 
-  max_hz = 100;
-  min_hz = 30;
-  min_hz = max(min_hz, 2*data.samplerate/windowsize);
-  clip_index = round(data.samplerate/max_hz);
+  clip_index = max(1, round(data.samplerate/max_hz));
   C(1:clip_index, :) = 0;
-  clip_index = round(data.samplerate/min_hz);
+  clip_index = max(1, round(data.samplerate/min_hz));
   clip_index = min(clip_index, windowsize/2);
   C(clip_index:end, :) = 0;
 
@@ -42,31 +55,67 @@ for channel=1:size(data.buffer,2)
   hz = data.samplerate ./ j;
   x = linspace(data.offset + windowsize/2, data.offset + size(data.buffer,1) - windowsize/2, numel(hz));
   t = x / data.samplerate;
-  a = min(1, a * (200/windowsize));
-  %a = min(1, 2*a ./ sum(abs(C)));
+  a = max(0.3, min(1, 100*a ./ sum(abs(C))));
 
   % Plot which peak that was found
-  %%data.plot(:,:,channel*2 -1 ) = [t' hz' a'];
-  data.plot(:,:,channel ) = [t' hz' a'];
+  %data.plot(:,:,channel*2 ) = [t' hz' a'];
 
-  % Fetch pattern
+  % For each window
+  hz2 = zeros(size(hz));
+  for k=1:numel(hz)
+    if isempty(state.hz)
+      state.hz = hz(1);
+    else
+      state.hz = state.hz*0.95 + hz(k)*0.05;
+    end
+    hz2(k) = state.hz;
+  end
+
+  % Fetch overtone number 4 and 8
+  overtones = [4 8];
+
   % Indicies in fourier domain
-%  k = windowsize*hz/data.samplerate;
+  k = windowsize*hz/data.samplerate;
+  n = overtones'*k;
 
-%  n = round([1:10]'*k);
-%hz
+  y = abs(ft(:));
+  offs = ones(1,size(n,1))'*(0:size(ft,2)-1)*size(ft,1);
+  m = round(n);
+  x = n - m;
+  y1 = y(m-1+offs);
+  y2 = y(m+offs);
+  y3 = y(m+1+offs);
+  
+  %solve
+  %y1 = k+p+q;
+  %y2 = q;
+  %y3 = k-p+q;
 
-%  y = ft(:);
-%  offs = ones(1,size(n,1))'*(0:size(ft,2)-1)*size(ft,1);
-%  pattern = y(n + offs);
+  p=(y1-y3)./2;
+  q=y2;
+  k=y1 + y3 - 2*q;
 
-  % Plot if pattern is changing
+  pattern = k.*x.*x + p.*k + q; 
+%  pattern = (1-b).*y(m + offs) + b.*y(m + offs + 1);
+
+  % Plot where pattern is changing the most
 %  patterndiff = pattern(:, 2:end)-pattern(:, 1:end-1);
 %  patterndiff(:,end+1) = patterndiff(:,end);
-%  [v,m]=max(patterndiff);
-%  hz2 = m/windowsize*data.samplerate;
+%  [v,m]=min(pattern);
+  v=min(pattern);
+%  hz2 = 2.*k/windowsize*data.samplerate;
+  a = zeros(size(a));
+  % For each window
+  for k=1:numel(hz)
+    state.e = state.e*0.95;
+    if v<0.5
+      state.e = min(2, state.e + 0.2);
+    end
 
-%hz2
+    a(k) = state.e > 0.5;
+  end
 
- % data.plot(:,:,channel*2 +1 ) = [t' hz2' v'];
+  data.plot(:,:,channel*2-1 ) = [t' hz2' a'];
+%  data.plot(:,:,channel*2-1 ) = [t' hz2' v'];
 end
+
--- matlab/getdatainfo.m
+++ matlab/getdatainfo.m
@@ -11,12 +11,13 @@ format long
 
 txt = ['#' num2str(state.counter) ' - ' ...
        'data = [' num2str(data.offset/data.samplerate) ', ' num2str((data.offset+size(data.buffer,1))/data.samplerate) ') s ' ...
-       '[' num2str(data.offset) ', ' num2str(data.offset+size(data.buffer,1)) ')'];
-if  0 ~= data.redundancy
-    txt = [ txt ' redundancy = ' num2str(data.redundancy) ];
-end
+       num2str(size(data.buffer,1)) ' samples'];
 
 if  1 ~= size(data.buffer,2)
     txt = [ txt ' channels=' num2str(size(data.buffer,2)) ];
+end
+
+if  0 ~= data.redundancy
+    txt = [ txt ' redundancy = ' num2str(data.redundancy) ];
 end
 
