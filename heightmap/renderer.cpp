80d065c5368f9df316d2ca928472663089305387
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-03-27 12:16:23 +0200

    Fast in 3D

--- heightmap/renderer.cpp
+++ heightmap/renderer.cpp
@@ -267,8 +267,8 @@ void Renderer::init()
     // load shader
     _shader_prog = loadGLSLProgram(":/shaders/heightmap.vert", ":/shaders/heightmap.frag");
 
-    setSize( collection->samples_per_block(), collection->scales_per_block() );
-    //setSize( collection->samples_per_block()/8, collection->scales_per_block()/2 );
+    //setSize( collection->samples_per_block(), collection->scales_per_block() );
+    setSize( collection->samples_per_block()/16, collection->scales_per_block() );
 
     //setSize(2,2);
 
