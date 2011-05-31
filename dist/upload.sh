0afb553cbc2ceab6c7aa126916d41fb802f07eb7
wannabag <patrick.juhl@reep.se>
2011-05-31 13:30:17 +0200

    Path correction

--- dist/upload.sh
+++ dist/upload.sh
@@ -13,7 +13,7 @@ mkdir $version
 cd $version
 binary
 $passiveftp
-put $filename" | ftp -n -v ftp.sonicawe.com
+put package-win\/$filename" | ftp -n -v ftp.sonicawe.com
 echo "Uploaded file to:"
 url="http://data.sonicawe.com/${version}/${filename}"
 echo $url
