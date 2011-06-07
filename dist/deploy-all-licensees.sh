08865db9de5b69457a4fdb4cbdf1d4dc7c6d8efb
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-06-07 22:27:39 +0200

    List licensees

--- dist/deploy-all-licensees.sh
+++ dist/deploy-all-licensees.sh
@@ -3,25 +3,36 @@ set -e
 
 urls=
 
-read -p "Rebuild all code? (Y/n) " simpleclean; echo
-if [ "${simpleclean}" == "y" ] || [ "${simpleclean}" == "Y" ]; then
-simpleclean=
-fi
-
 INPUT=licensees.txt
 OLDIFS=$IFS
 
 # IFS is set to 'tab'
 IFS="	"
 
+echo "Licensee's:"
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read personalemail personallicensetype personalexpired target
+cat $INPUT | while read personalemail personallicensetype personalexpired target
 do
+	echo "email: '$personalemail'	license type: '$personallicensetype'	license expires: '$personalexpired'	license target: '$target'"
+done
+IFS=$OLDIFS
+
+personal=y
+
+IFS="	"
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read personalemail personallicensetype personalexpired target < $INPUT
+do
+	if [ -z "$personalemail" ]; then
+		continue;
+	fi
+
 	IFS=$OLDIFS
-	. ./deploy.sh; urls="${urls}\n$url"
-	simpleclean=nonempty
+	. ./deploy.sh
+	urls="${urls}\n$url"
+	rebuildall="n"
 	IFS="	"
-done < $INPUT
+done
 IFS=$OLDIFS
 
 
