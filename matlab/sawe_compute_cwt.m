252dc2fee5cd947383bb656b3cbb2dcbf5265f64
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-06-29 17:00:31 +0200

    added sawe_compute_cwt

---
+++ matlab/sawe_compute_cwt.m
@@ -0,0 +1,20 @@
+% computes CWT of a signal
+function s_cwt = sawe_compute_cwt(X)
+
+	s_cwt = [];
+	soundfile = tempname();
+	wavwrite(X, 44100, soundfile);
+
+	size(X,1)/(2
+	for k=0:(ceil(log2(size(X,1)/13));
+		system(['sonicawe --samples_per_chunk_hint=13 --get_hdf=' num2str(k) ' "' soundfile '"' ]);
+        outputfile = ['save_chunk-' num2str(k) '.h5'];
+        data = sawe_loadchunk(outputfile);
+        delete(outputfile);
+		s_cwt_one = flipud((data.chunk)');
+		s_cwt = [s_cwt s_cwt_one];
+	end
+
+    delete(soundfile);
+end
+
