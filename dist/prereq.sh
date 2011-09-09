95bc5ac08f15e97fda116fc2bbfdafbb4c7a7351
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-09-09 05:01:06 +0200

    Verify repositories? (Y/n)

--- dist/prereq.sh
+++ dist/prereq.sh
@@ -12,28 +12,38 @@ else
   snapshot="-test-$sonicawe_branch"
 fi
 
-echo "================= Checking local repo status =================="
-cd ../../gpumisc
-if [ -n "$(git status -uno --porcelain)" ]; then echo "In gpumisc: local git repo is not clean."; exit 1; fi
-cd ../sonicawe
-if [ -n "$(git status -uno --porcelain)" ]; then echo "In sonicawe: local git repo is not clean."; exit 1; fi
-
 echo "===================== Deploying Sonic AWE ====================="
 echo "branch: ${branch}"
 echo "version: ${version}"
 echo "release: sonicawe_${version}${snapshot}"
 
+read -p "Verify repositories? (Y/n) " verifyRepos; echo
+if [ "Y" == "${verifyRepos}" ] || [ "y" == "${verifyRepos}" ]; then
+	verifyRepos=;
+fi
+if [ -z "${verifyRepos}" ]; then
+	cd ../../gpumisc
+	if [ -n "$(git status -uno --porcelain)" ]; then echo "In gpumisc: local git repo is not clean."; exit 1; fi
+	cd ../sonicawe
+	if [ -n "$(git status -uno --porcelain)" ]; then echo "In sonicawe: local git repo is not clean."; exit 1; fi
+	cd dist
+fi
+
 if [ -z "${rebuildall}" ]; then read -p "Rebuild all code? (Y/n) " rebuildall; echo; fi
+
 
 read -s -p "Enter password for ftp.sonicawe.com: " pass; echo
 if [ -z "$pass" ]; then echo "Missing password for ftp.sonicawe.com, can't deploy."; exit 1; fi
 
-echo "==================== Updating local repos ====================="
-git pull --rebase origin $branch
-git fetch origin
+if [ -z "${verifyRepos}" ]; then
+	echo "==================== Updating local repos ====================="
+	cd ../../gpumisc
+	git pull --rebase origin master
+	git fetch origin
 
-cd ../gpumisc
-git pull --rebase origin master
-git fetch origin
+	cd ../sonicawe
+	git pull --rebase origin $branch
+	git fetch origin
 
-cd ../sonicawe/dist
+	cd dist
+fi
