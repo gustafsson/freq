dda506da216d1915ab3ee1492d4d42417edd5ad7
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-11-05 19:14:10 +0100

    Rectangle kernel works on cpu

---
+++ filters/rectanglekernel_cpu.cpp
@@ -0,0 +1,18 @@
+#include "resamplecpu.h"
+#include "rectanglekerneldef.h"
+
+void removeRect( Tfr::ChunkData::Ptr waveletp, Area area, bool save_inside )
+{
+    Tfr::ChunkElement* wavelet = CpuMemoryStorage::ReadWrite<2>( waveletp ).ptr();
+    DataStorageSize size = waveletp->size();
+
+    DataPos p(0,0);
+    for (p.y=0; p.y<size.height; ++p.y)
+    {
+        for (p.x=0; p.x<size.width; ++p.x)
+        {
+            remove_rect_elem(p, wavelet, size, area, save_inside );
+        }
+    }
+}
+
