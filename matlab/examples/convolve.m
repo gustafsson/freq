a8bf5627025a91e14a014bad10e6933209c4d966
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-11-14 19:12:01 +0100

    convolve.m works for multichanneled data

--- matlab/examples/convolve.m
+++ matlab/examples/convolve.m
@@ -2,9 +2,14 @@ function [data]=convolve(data)
 
 disp(['convolve ' sawe_getdatainfo(data)]);
 
-filter=[1 -1 1];
+filter=[1 -1 1]';
 %filter=filter/abs(sum(filter));
-data.samples = conv(data.samples, filter);
+for channel=1:size(data.samples,2)
+  convdata(:,channel) = conv(data.samples(:,channel), filter);
+end
+
+data.samples = convdata;
+% play around
 data.samples = data.samples + rand(size(data.samples));
 
 data.overlap = 5;
--- matlab/examples/plotwaveform.m
+++ matlab/examples/plotwaveform.m
@@ -2,7 +2,7 @@ function [data]=plotwaveform(data)
 
 disp(['plotwaveform ' sawe_getdatainfo(data)]);
 
-for channel=1:1 %size(data.samples,2)
+for channel=1:size(data.samples,2)
   x = (1:100:size(data.samples,1));
   t = (data.offset + x) / data.fs;
   hz = 1000 + 1000*data.samples(x, channel);
