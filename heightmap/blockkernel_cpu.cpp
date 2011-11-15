0407ecd84daf5c4cb6e90fec577dd15bd061fdd8
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-11-15 21:17:57 +0100

    undef FREQAXIS_CALL from precompiled header

--- heightmap/blockkernel_cpu.cpp
+++ heightmap/blockkernel_cpu.cpp
@@ -1,4 +1,10 @@
 #ifndef USE_CUDA
+
+#if defined(_MSC_VER) && defined(FREQAXIS_CALL)
+// defined in precompiled header
+#undef FREQAXIS_CALL
+#endif
+
 #include <resamplecpu.h>
 
 #include <complex>
