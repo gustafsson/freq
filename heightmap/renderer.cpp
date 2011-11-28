51183b9f04496e51a051ca0558b89394b74fa639
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-11-28 02:23:06 +0100

    renderer redundancy 1.f

--- heightmap/renderer.cpp
+++ heightmap/renderer.cpp
@@ -51,7 +51,7 @@ Renderer::Renderer( Collection* collection )
     _mesh_fraction_height(1),
     _initialized(NotInitialized),
     _draw_flat(false),
-    _redundancy(0.75f), // 1 means every pixel gets at least one texel (and vertex), 10 means every 10th pixel gets its own vertex, default=2
+    _redundancy(1.f), // 1 means every pixel gets at least one texel (and vertex), 10 means every 10th pixel gets its own vertex, default=2
     _invalid_frustum(true)
 {
     memset(modelview_matrix, 0, sizeof(modelview_matrix));
