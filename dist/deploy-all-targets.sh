b3b3222b69f8c912b3a009d29162fa6bed2290e8
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-12-06 13:04:28 +0100

    Only create tags for uploaded data

--- dist/deploy-all-targets.sh
+++ dist/deploy-all-targets.sh
@@ -20,26 +20,28 @@ done
 
 echo
 
-tagname=$versiontag-$platform
-if [ "$(uname -s)" == "Linux" ]; then tagname=${tagname}_`uname -m`; fi
-
-if [ -n "`git tag | grep $tagname`" ]; then
-	echo "Tag '$tagname' already exists at `git show-ref -s --abbrev $tagname`. Removing old tag and creating a new one"
-	git tag -d $tagname
-	git push origin :$tagname || :
-fi
-
-git tag $tagname
-git push origin $tagname || :
-echo "Created tag '$tagname' (at `git show-ref -s --abbrev $tagname`)"
-
-echo
-
-echo "========================== All urls ==========================="
 if [ -z "`echo -e $a`" ] && [ -z "$pass" ]; then
 	echo "Didn't upload anything (no ftp password was given)"
+
 elif [ -z "`echo -e $a`" ]; then
 	echo "Didn't upload anything (unavailable internet connection? wrong ftp password?)"
+
 else
+	tagname=$versiontag-$platform
+	if [ "$(uname -s)" == "Linux" ]; then tagname=${tagname}_`uname -m`; fi
+
+	if [ -n "`git tag | grep $tagname`" ]; then
+		echo "Tag '$tagname' already exists at `git show-ref -s --abbrev $tagname`. Removing old tag and creating a new one"
+		git tag -d $tagname
+		git push origin :$tagname || :
+	fi
+
+	git tag $tagname
+	git push origin $tagname || :
+	echo "Created tag '$tagname' (at `git show-ref -s --abbrev $tagname`)"
+
+	echo
+
+	echo "========================== All urls ==========================="
 	echo -e $urls
 fi
