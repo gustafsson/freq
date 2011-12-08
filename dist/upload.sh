6a463ef530371563b7f8a5127d4923a1cf5dd6c0
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-12-08 11:10:01 +0100

    Windows ftp command is unreliable

--- dist/upload.sh
+++ dist/upload.sh
@@ -5,6 +5,8 @@ if [ -z "${version}" ]; then echo "Missing version, can't upload."; exit 1; fi
 if [ -z "${filename}" ]; then echo "Missing filename, can't upload."; exit 1; fi
 if [ -z "$pass" ]; then echo "Missing password, skipping upload."; return; fi
 
+if [ "$(uname -s)" == "MINGW32_NT-6.1" ]; then echo "Windows ftp command is unreliable. Please upload manually instead."; return fi
+
 echo "======================== Uploading to ftp ========================"
 echo "Connecting..."
 time (echo "user sonicawe.com $pass
