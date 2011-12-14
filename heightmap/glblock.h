973980100c6f68eab5ea9d535b6a7ae9625b9b6c
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-12-14 17:38:25 +0100

    Change samples_per_block_ to 128 instead of 256

--- heightmap/glblock.h
+++ heightmap/glblock.h
@@ -8,10 +8,10 @@
 #include <stdio.h>
 #include <complex>
 
-//#define BLOCK_INDEX_TYPE GL_UNSIGNED_SHORT
-//#define BLOCKindexType GLushort
-#define BLOCK_INDEX_TYPE GL_UNSIGNED_INT
-#define BLOCKindexType GLuint
+#define BLOCK_INDEX_TYPE GL_UNSIGNED_SHORT
+#define BLOCKindexType GLushort
+//#define BLOCK_INDEX_TYPE GL_UNSIGNED_INT
+//#define BLOCKindexType GLuint
 
 
 
