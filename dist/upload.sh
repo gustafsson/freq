3404cc8d59bcbd826b759d639f31465b174dac4c
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2011-12-30 13:30:57 +0100

    Uploading with curl instead of ftp command

--- dist/upload.sh
+++ dist/upload.sh
@@ -6,3 +6,7 @@ if [ -z "${filename}" ]; then echo "Missing filename, can't upload."; exit 1; fi
 if [ -z "$pass" ]; then echo "Missing password, skipping upload."; return; fi
 
 curl -T "$filename" -u sonicawe.com:$pass --ftp-create-dirs ftp://ftp.sonicawe.com/data/$version/
+
+echo "Uploaded file to:"
+url="http://data.sonicawe.com/${version}/${filename}"
+echo $url
