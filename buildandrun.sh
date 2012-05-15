4f584a181ecfd700321e7e3093165a79381a3407
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2012-05-15 22:26:39 +0200

    Check submodules in prereq.sh instead of buildandrun.sh

--- buildandrun.sh
+++ buildandrun.sh
@@ -3,45 +3,11 @@ set -e
 
 echo "Set a variable named 'target' to build for other targets than the default (see dist/deploy-all-targets.sh)"
 
-# Check if git user name has been specified
-if [ -z "`git config --global user.name`" ]; then
-    echo "git has not yet been configured. Configure now or press ^C"
-    read -p "Please enter your name, such as Donald Duck: " username
-    read -p "Please enter your email, such as donald@duck.com: " usermail
-    git config --global user.name "$username"
-    git config --global user.email "$usermail"
-fi
-
-# Enforce this (being a bit intrusive here)
-git config merge.ff false
-
-# Check if the submodules have been initialized
-if ! grep submodule .git/config > /dev/null; then
-    git submodule update --init lib/gpumisc
-    
-    if [ "$(uname -s)" == "MINGW32_NT-6.1" ]; then
-        git submodule update --init lib/sonicawe-winlib
-    elif [ "$(uname -s)" == "Linux" ]; then
-        if [ -z `which colorgcc` ]; then
-            echo "Some required and recommended libraries seem to be missing, running apt-get"
-            glewpkg=libglew1.6-dev
-            if [ -z `apt-cache search $glewdeb` ]; then
-                glewpkg=libglew1.5-dev
-            fi
-            sudo apt-get install libsndfile1-dev portaudio19-dev $glewpkg freeglut3-dev libboost-dev libboost-serialization-dev libqt4-dev qtcreator libhdf5-serial-dev qgit build-essential colorgcc git-gui git-doc
-        fi
-    elif [ "$(uname -s)" == "Darwin" ]; then
-        git submodule update --init lib/sonicawe-maclib
-    else
-        echo "Don't know how to build Sonic AWE for this platform: $(uname -s).";
-    fi
-fi
-
-sonicawebranch=`git rev-parse --abbrev-ref HEAD`
-
 cd dist
 . ./deploy.sh
 cd ..
+
+sonicawebranch=`git rev-parse --abbrev-ref HEAD`
 
 echo "Running Sonic AWE with sonicawe@${sonicawebranch}"
 if [ "$(uname -s)" == "MINGW32_NT-6.1" ]; then
--- dist/prereq.sh
+++ dist/prereq.sh
@@ -17,6 +17,18 @@ echo "branch: ${branch}"
 echo "version: ${version}"
 echo "release: sonicawe_${version}${snapshot}"
 
+# Check if git user name has been specified
+if [ -z "`git config --global user.name`" ]; then
+	echo "git has not yet been configured. Configure now or press ^C"
+	read -p "Please enter your name, such as Donald Duck: " username
+	read -p "Please enter your email, such as donald@duck.com: " usermail
+	git config --global user.name "$username"
+	git config --global user.email "$usermail"
+fi
+
+# Enforce this (being a bit intrusive here)
+git config merge.ff false
+
 read -p "Verify and update repositories? (Y/n) " verifyRepos; echo
 if [ "N" == "${verifyRepos}" ] || [ "n" == "${verifyRepos}" ]; then
 	verifyRepos=N;
@@ -51,6 +63,31 @@ then
     exit
 fi
 
+# Check if the submodules have been initialized
+(
+	cd ..
+
+	if ! grep submodule .git/config > /dev/null; then
+		git submodule update --init lib/gpumisc
+
+		if [ "$(uname -s)" == "MINGW32_NT-6.1" ]; then
+			git submodule update --init lib/sonicawe-winlib
+		elif [ "$(uname -s)" == "Linux" ]; then
+			if [ -z `which colorgcc` ]; then
+				echo "Some required and recommended libraries seem to be missing, running apt-get"
+				glewpkg=libglew1.6-dev
+				if [ -z `apt-cache search $glewdeb` ]; then
+					glewpkg=libglew1.5-dev
+				fi
+				sudo apt-get install libsndfile1-dev portaudio19-dev $glewpkg freeglut3-dev libboost-dev libboost-serialization-dev libqt4-dev qtcreator libhdf5-serial-dev qgit build-essential colorgcc git-gui git-doc
+			fi
+		elif [ "$(uname -s)" == "Darwin" ]; then
+			git submodule update --init lib/sonicawe-maclib
+		else
+			echo "Don't know how to build Sonic AWE for this platform: $(uname -s).";
+		fi
+	fi
+) || false
 
 if [ "Y" == "${verifyRepos}" ]; then
 	echo "==================== Updating local repos ====================="
