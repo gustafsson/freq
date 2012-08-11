1a846ea3793e717d3546f694109b0c9ef377d929
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2012-08-11 17:20:54 +0200

    Added Command class documentation.

--- src/tools/commands/command.h
+++ src/tools/commands/command.h
@@ -7,6 +7,14 @@
 namespace Tools {
 namespace Commands {
 
+/**
+ * @brief The Command class represents user actions.
+ *
+ * The Command class is intended to be the interface towards changing the
+ * user-level state of the application. Commands are invoked through
+ * CommandInvoker::invokeCommand(pCommand). CommandInvoker keeps track of a
+ * CommandList that describes the order of executed and undone commands.
+ */
 class Command
 {
 public:
@@ -16,24 +24,51 @@ public:
     virtual std::string toString() = 0;
 
 private:
+    /**
+     Only class CommandList calls Command methods.
+     */
     friend class CommandList;
 
+
     /**
-      If two similiar Commands are executed right after each other they
-      might be melded into one only command.
+      @breif meldPrevCommand stores the undo-state of another Command.
 
-      Typically used for small incremental changes within a short time interval.
+      If two similiar Commands are executed right after each other they might
+      be melded into one only command. This is typically used for small
+      incremental changes within a short time interval.
 
-      meldPrevCommand is only called right before this command is executed.
+      'meldPrevCommand' is only called right after this command is executed.
       This command will be executed regardless of the return value of meldPrevCommand.
 
       When a command is being redone, meldPrevCommand is not called.
+
+      @return If meldPrevCommand returns true 'prev' will be removed from the
+      command list. And calling this->undo() should restore the state that was
+      prev->undo() would have restored.
       */
-    virtual bool meldPrevCommand(Command*) { return false; }
+    virtual bool meldPrevCommand(Command* /*prev*/) { return false; }
+
+
+    /**
+      @brief addToList describes whether this Command should go into the
+      command history listing.
+      @return true if this command should go into the command history listing.
+     */
     virtual bool addToList() { return true; }
 
 
+    /**
+      @brief execute performs this command. execute() is called when the
+      command is invoked. execute() can also be called again if the command
+      has been undone by calling undo().
+     */
     virtual void execute() = 0;
+
+
+    /**
+      @brief undo restores the state that was changed by execute().
+      @see execute()
+     */
     virtual void undo() = 0;
 };
 
