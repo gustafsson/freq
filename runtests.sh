6e5379a60161e644b315df4f94592257b1cbbf84
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2012-09-02 17:15:34 +0200

    ./runtests.sh works well together with git bisect run runtests.sh updates submodules, touches all source code to force rebuild, and executes tests/runtests.sh

---
+++ runtests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Useful when bisecting like so:
+# git bisect start
+# git bisect good
+# git bisect bad
+# git bisect run ./runtests.sh
+
+if [ `dirname $0` != "." ]; then
+  echo "Run from sonicawe root folder"
+  false
+fi
+
+if [ -d .git/refs/bisect ]; then
+  git submodule update
+fi
+
+touch `find . -name *.cpp` `find . -name *.cu` `find . -name *.pro`
+
+tests/runtests.sh
