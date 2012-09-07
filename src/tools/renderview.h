18120091823007a871599f602220ab1bddfa938d
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2012-09-07 15:09:25 +0200

    Added suggestions for connecion type to RenderView signals.

--- src/tools/renderview.h
+++ src/tools/renderview.h
@@ -101,6 +101,7 @@ namespace Tools
 
     signals:
         /**
+         * @brief destroying. Use 'Qt::DirectConnection'
           Emitted in the destructor, before the OpenGL context is destroyed.
           QObject::destroyed() is emitted shortly after, but after the OpenGL
           context is destroyed.
@@ -108,38 +109,57 @@ namespace Tools
         void destroying();
 
         /**
+         * @brief prePaint. Use 'Qt::DirectConnection'
           Emitted right before camera setup. A tool have the option to affect
           the renderview camera for this frame.
           */
         void prePaint();
 
         /**
+         * @brief painting. Use 'Qt::DirectConnection'
           Emitted during painting, but after the heightmap has been rendered.
           Tool specific stuff is rendered here.
           */
         void painting();
 
         /**
-          */
+         * @brief populateTodoList. Use 'Qt::DirectConnection'
+         */
         void populateTodoList();
 
         /**
-          */
+         * @brief postPaint. Use 'Qt::DirectConnection'
+         */
         void postPaint();
 
         /**
-          */
+         * @brief paintingForeground. Use 'Qt::DirectConnection'
+         */
         void paintingForeground();
 
         /**
-          */
+         * @brief finishedWorkSection. Use 'Qt::AutoConnection'
+         */
         void finishedWorkSection();
 
 
+        /**
+         * @brief postUpdate. Use 'Qt::DirectConnection'
+         */
         void postUpdate();
 
 
+        /**
+         * @brief transformChanged is emitted through emitTransformChanged.
+         * emitTransformChanged should be called whenever the state of the
+         * transform parameters have changed. This signal might be issued
+         * several times during a frame. Use 'Qt::QueuedConnection'.
+         */
         void transformChanged();
+
+        /**
+         * @brief axisChanged. Use 'Qt::QueuedConnection'
+         */
         void axisChanged();
 
     private slots:
