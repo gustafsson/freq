bced0539a57766a5af43aed1572c085d8b42a42b
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2013-08-11 16:29:01 +0200

    Tidy

--- src/tools/support/transformdescs.cpp
+++ src/tools/support/transformdescs.cpp
@@ -7,8 +7,8 @@ namespace Support {
 Tfr::TransformDesc::Ptr TransformDescs::
         cloneType(const std::type_info& i) const
 {
-    foreach(Tfr::TransformDesc::Ptr p, descriptions_)
-        if (typeid(*p.get()) ==i)
+    foreach (Tfr::TransformDesc::Ptr p, descriptions_)
+        if (typeid(*p.get()) == i)
             return p->copy ();
     return Tfr::TransformDesc::Ptr();
 }
