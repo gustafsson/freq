8d378a04d518f78af0c13c121032fe819723fdf7
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2014-04-09 11:21:03 +0200

    autofeedback

--- src/sawe/configuration/configuration.cpp
+++ src/sawe/configuration/configuration.cpp
@@ -697,6 +697,8 @@ void Configuration::
     features_.push_back("transform_info");
     features_.push_back("timeline_dock");
     features_.push_back("allow_unregistered_start");
+    features_.push_back("autofeedback");
+
     if (!DetectGdb::is_running_through_gdb() && !DetectGdb::was_started_through_gdb ())
         features_.push_back("logfile");
 }
