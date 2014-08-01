acec50e4a7e428a9f7e08184c44c0119ad0a27aa
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2014-08-01 18:55:09 +0200

    uncaughtexception: log with mutex

--- lib/heightmap/heightmap/uncaughtexception.cpp
+++ lib/heightmap/heightmap/uncaughtexception.cpp
@@ -1,16 +1,19 @@
 #include "uncaughtexception.h"
-#include "tasktimer.h"
+#include "log.h"
 
 namespace Heightmap {
 
 std::function<void(boost::exception_ptr)> UncaughtException::handle_exception =
         [](boost::exception_ptr x)
         {
+            std::string info = boost::current_exception_diagnostic_information ();
+            Log("!!! %s") % info;
+
             fflush(stdout);
             fprintf(stderr, "%s",
                     str(boost::format("\nUncaught exception\n"
                                       "%s\n\n")
-                        % boost::current_exception_diagnostic_information ()).c_str());
+                        % info).c_str());
             fflush(stderr);
 
             boost::rethrow_exception (x);
