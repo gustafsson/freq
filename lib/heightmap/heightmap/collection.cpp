5c4195228d043b1f6759a7674d94e4d752c29c2d
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2014-08-21 18:34:46 +0200

    collection: don't bother about the "background color"

--- lib/heightmap/heightmap/collection.cpp
+++ lib/heightmap/heightmap/collection.cpp
@@ -260,12 +260,6 @@ void Collection::
 int Collection::
         runGarbageCollection(bool aggressive)
 {
-    {
-        // Make sure this global block covering everything is available to provide a background color
-        pBlock b = getBlock(entireHeightmap ());
-        b->frame_number_last_used = _frame_counter;
-    }
-
     Blocks::GarbageCollector gc(cache_);
     unsigned F = gc.countBlocksUsedThisFrame(_frame_counter);
     unsigned max_cache_size = 4*F;
