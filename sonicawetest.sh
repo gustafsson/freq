c42a0df690ec02c688f6a9cb80d1dac73d144e68
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2014-08-22 00:50:22 +0200

    sonicawetest: send output to tmp files

--- sonicawetest.sh
+++ sonicawetest.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 qmake -r
+rm -f src/sonicawe
 make -j12 || exit 125
+
+mkdir -p tmp/sonicawetest
 cd src
-./sonicawe --test
+HEAD=`git rev-parse HEAD`
+./sonicawe --test >& ../tmp/sonicawetest/$HEAD
 exit $?
